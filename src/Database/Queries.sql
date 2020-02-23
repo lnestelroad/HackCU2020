c44cc35914db7add6bf89237c184783d5adc0aee
liam <liam@pop.os>
2020-02-22 18:42:57 -0700

    Changing docker to a compose file

--- src/Database/Queries.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS Foods(
-    FoodID SERIAL PRIMARY KEY,
-    FoodName VARCHAR(100) NOT NULL,
-    FoodType VARCHAR (100) NOT NULL,
-    ShelfLife DATE,
-    Present BOOL,
-    Parishable BOOL
-);
