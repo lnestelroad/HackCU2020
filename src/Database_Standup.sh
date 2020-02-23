c3a7519690de5be104d8c866d99caa3db0016ce9
tlincke125 <thli2739@colorado.edu>
2020-02-22 21:01:15 -0700

    added prettified

---
+++ src/Database_Standup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# docker network create postgres_backend 
+# docker run --name postgres \
+#   -e POSTGRES_PASSWORD=liam \
+#   -e POSTGRES_USER=Liam \
+#   -e POSTGRES_DB=HackCU \
+#   -d --restart always \
+#   -v postgres:/var/lib/postgresql/data \
+#   --net postgres_backend \
+#   postgres:9.6
+# docker-compose up -d
+
+IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' database_db_1)
+
+python3 test.py -i $IP
--- src/foodbank/data_update.py
+++ src/foodbank/data_update.py
@@ -1,6 +1,7 @@
 from Database import Database
+from foodbank import DistanceMatrix
 
-def db_parse_to_json(ip, build):
+def db_parse_to_json(ip):
 
     DM = DistanceMatrix()
 
@@ -11,6 +12,13 @@ def db_parse_to_json(ip, build):
     r_addresses = interface.getResterauntAddresses()
     d_addresses = interface.getDonationCenterAddresses()
 
-    for i in 
+    for i in r_addresses:
+        print(type(i[0]))
+        DM.push(i[0], identification=i[0], d_type='o')
+
+    for i in d_addresses:
+        DM.push(i[0], identification=i[0], d_type='d')
+
+    DM.to_json_file("./.cache_money/graph.json")
     
     
